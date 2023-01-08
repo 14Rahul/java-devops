209491555ffe05ad42782f77c3be8127221b84b8
14Rahul <rahulgupta141998@gmail.com>
2023-01-08 20:18:38 +0530

    Auto Deploy

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello from Spring Boot j!";
+        return "Hello from Spring Boot D!";
     }
 
     public static void main(String[] args) {
