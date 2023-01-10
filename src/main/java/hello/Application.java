ab639faf68df21a337930c27dfeef9393bbfb3ca
14Rahul <rahulgupta141998@gmail.com>
2023-01-10 23:33:07 +0530

    Nexus Commit4

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello from Spring Boot D5!";
+        return "Hello from Spring Boot D6!";
     }
 
     public static void main(String[] args) {
