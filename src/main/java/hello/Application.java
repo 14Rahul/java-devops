c9b41f1beddaf8e7e93dc7b590355940ec2edbde
14Rahul <48749887+14Rahul@users.noreply.github.com>
2023-01-10 21:39:12 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello from Spring Boot D1!";
+        return "Hello from Spring Boot D2!";
     }
 
     public static void main(String[] args) {
